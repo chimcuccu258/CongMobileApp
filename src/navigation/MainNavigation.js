b348036f39b874437adb3494fc2e51c8af0d521f
chimcuccu258 <vuminhnga25@gmail.com>
2023-08-29 08:58:16 +0700

    2908

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -1,44 +1,46 @@
-import {View, Text} from 'react-native';
 import React from 'react';
-import Home from '../screens/Home';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Home from '../screens/Home';
 import Order from '../screens/Order';
+import Login from '../screens/Login';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
-const MainNavigation = () => {
+const Tabs = () => {
   return (
     <Tab.Navigator
-      screenOptions={({route}) => ({
-        tabBarIcon: ({focused, color, size}) => {
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
           let iconName;
 
           if (route.name === 'Tab1') {
-            iconName = focused
-              ? 'information'
-              : 'information-outline';
+            iconName = focused ? 'home-variant' : 'home-variant-outline';
           } else if (route.name === 'Tab2') {
-            iconName = focused ? 'information' : 'information-outline';
+            iconName = focused ? 'cart' : 'cart-outline';
           }
 
-          // You can return any component that you like here!
           return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: 'tomato',
+        tabBarActiveTintColor: 'green',
         tabBarInactiveTintColor: 'gray',
-      })}>
-      <Tab.Screen 
-        name="Tab1" 
-        component={Home} 
-        options={{headerShown: false}} 
-      />
-      <Tab.Screen
-        name="Tab2"
-        component={Order}
-        options={{headerShown: false}}
-      />
+      })}
+    >
+      <Tab.Screen name="Tab1" component={Home} options={{ headerShown: false }} />
+      <Tab.Screen name="Tab2" component={Order} options={{ headerShown: false }} />
     </Tab.Navigator>
+  );
+};
+
+const MainNavigation = () => {
+  return (
+    <Stack.Navigator initialRouteName='Login'>
+      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+      <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
+    </Stack.Navigator>
   );
 };
 
---
+++ src/screens/Login.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+// import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Login = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      {/* <Text>Login</Text> */}
+      <View style={styles.container}>
+        <Text>Welcome Back</Text>
+        <View>
+          
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  container: {
+    flex: 1,
+  }
+})
