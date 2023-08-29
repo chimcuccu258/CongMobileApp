a675372c9204ced57080e794cbae7187a1b638b7
chimcuccu258 <vuminhnga25@gmail.com>
2023-08-29 09:05:12 +0700

    29081

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-// import { View, Text, StyleSheet } from 'react-native'
 import { View, Text, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
